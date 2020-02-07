906f4aa12d278243c329ac30affce53fb9946f19
Robert Gandley <reg2763@cs.rit.edu>
2020-02-07 13:02:58 -0500

    modifications to Flask server

--- server.py
+++ server.py
@@ -1,5 +1,12 @@
 from os import environ
 from flask import Flask
 
-app = Flask("fishing.py")
-app.run(host= '0.0.0.0', port=environ.get('PORT'))
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    exec(open("fishing.py").read())
+
+if __name__ == "__main__":
+    app.run(host= '0.0.0.0', port=environ.get('PORT'))
+
