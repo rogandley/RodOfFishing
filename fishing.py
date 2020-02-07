55394da088c864cd8d3cbf99ca5e00f832149a99
Robert Gandley <reg2763@cs.rit.edu>
2020-02-07 11:46:49 -0500

    test config keys?

--- fishing.py
+++ fishing.py
@@ -4,10 +4,10 @@
 import tweepy, time, random, os, keys
 
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = keys.consumerK#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = keys.consumerS#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = keys.accessK#keep the quotes, replace this with your access token
-ACCESS_SECRET = keys.accessS#keep the quotes, replace this with your access token secret
+#CONSUMER_KEY = keys.consumerK#keep the quotes, replace this with your consumer key
+#CONSUMER_SECRET = keys.consumerS#keep the quotes, replace this with your consumer secret key
+#ACCESS_KEY = keys.accessK#keep the quotes, replace this with your access token
+#ACCESS_SECRET = keys.accessS#keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
